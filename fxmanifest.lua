e3ba57963f7e4fa8a6f09572c55b93ba0c64f967
Agent-Kwabbel <fabedgstuff@hotmail.com>
2022-10-29 21:10:25 +0200

    Setup

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Agent-Kwabbel - GelderlandRP'
+description 'Een cruise control systeem voor FiveM'
+version '1.0.0'
+
+client_script 'client.lua'
+
+server_script 'server.lua'
